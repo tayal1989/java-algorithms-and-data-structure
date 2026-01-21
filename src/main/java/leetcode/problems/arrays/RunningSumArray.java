1db1ac40cde9fe7c8010ef10bd7191260686e377
Vishal Agarwal <vishal.agarwal@zupee.in>
2026-01-21 18:23:55 +0530

    Added two more problems - max profit and running sum array

--- src/main/java/leetcode/problems/arrays/RunningSumArray.java
+++ src/main/java/leetcode/problems/arrays/RunningSumArray.java
@@ -1,4 +1,24 @@
 package leetcode.problems.arrays;
 
+import java.util.Arrays;
+
 public class RunningSumArray {
+
+    public static void main(String[] args) {
+        RunningSumArray runningSumArray = new RunningSumArray();
+        System.out.println(Arrays.toString(runningSumArray.runningSum(new int[]{1,2,3,4})));
+        System.out.println(Arrays.toString(runningSumArray.runningSum(new int[]{1,1,1,1,1})));
+        System.out.println(Arrays.toString(runningSumArray.runningSum(new int[]{3,1,2,10,1})));
+    }
+
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            sum = nums[i] + nums[i + 1];
+            nums[i + 1] = sum;
+        }
+
+        return nums;
+    }
 }
--- src/main/java/leetcode/problems/arrays/TimeToBuySellStock.java
+++ src/main/java/leetcode/problems/arrays/TimeToBuySellStock.java
@@ -1,4 +1,33 @@
 package leetcode.problems.arrays;
 
+import java.util.Arrays;
+
 public class TimeToBuySellStock {
+
+    public static void main(String[] args) {
+        TimeToBuySellStock timeToBuySellStock = new TimeToBuySellStock();
+        System.out.println(timeToBuySellStock.maxProfit(new int[] {7,1,5,3,6,4}));
+        System.out.println(timeToBuySellStock.maxProfit(new int[] {7,6,4,3,1}));
+        System.out.println(timeToBuySellStock.maxProfit(new int[] {2,4,1}));
+    }
+
+    public int maxProfit(int[] prices) {
+        int output = 0;
+        int minElement = prices[0];
+        int index = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            if(minElement > prices[i]) {
+                minElement = prices[i];
+                index = i;
+            }
+
+            int maxDiff = prices[i] - prices[index];
+            if(output < maxDiff) {
+                output = maxDiff;
+            }
+        }
+
+        return output;
+    }
 }
